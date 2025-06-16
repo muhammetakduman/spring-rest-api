466f6dd1ac26a9fe6aa3b1a840405f944d996537
Muhammet Akduman <akdumanmxc@gmail.com>
2025-06-16 20:38:52 +0300

    have parameters http

--- src/main/java/com/muhammetakduman/controller/RestEmployeeController.java
+++ src/main/java/com/muhammetakduman/controller/RestEmployeeController.java
@@ -4,30 +4,34 @@ package com.muhammetakduman.controller;
 import com.muhammetakduman.model.Employee;
 import com.muhammetakduman.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.Path;
 import java.util.List;
 
 @RestController
 //Kök Path
-@RequestMapping("/rest/api")
+@RequestMapping("/rest/api/employee")
 public class RestEmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping(path = "/employee-list")
+    @GetMapping(path = "/list")
     public List<Employee> getAllEmployeeList(){
         return employeeService.getAllEmplyoeeList();
 
     }
 
-    @GetMapping(path = "/employee-list/{id}")
+    @GetMapping(path = "/list/{id}")
     public Employee getEmplyoeeById(@PathVariable(name = "id", required = true) String id){
         return employeeService.getEmplyoeeById(id);
     }
+
+    @GetMapping(path = "employee-list-with-params")
+    public List<Employee> getEmployeeWithParams(@RequestParam(name = "firstName") String firstName,
+                                                @RequestParam(name ="lastName") String lastName){
+        return null;
+    }
+
 }
